717ca4108dc58d6d0c6bd62f6311bb56c2381d2f
sartaj <sartajs2002@gmail.com>
2023-12-16 16:51:56 -0500

    no preprocessing. trying to make it work

--- mysh.c
+++ mysh.c
@@ -2,16 +2,7 @@
 
 #define DELIMS " <>|"
 
-struct block {
-	int num_args;
-	int num_comm;
-	char *args[12];
-	char *commargs[12];
-	bool output_re;
-	bool append;
-	bool input_re;
-	char *file;
-};
+int execute_program(char *curr_program, int other_side);
 
 int main(int argc, char *argv[]) {
 
@@ -24,10 +15,10 @@ int main(int argc, char *argv[]) {
 		//current_dir = current_dir;
 	}	
 
-
 	while (true) {
 		char input[4096];
-
+		int num_programs = 1;
+		
 		printf("[%s]# ", current_dir);
 		if (fgets(input, 4096, stdin) == NULL) { //use fgets to store the command in a string
 			return -1;
@@ -35,29 +26,439 @@ int main(int argc, char *argv[]) {
 	
 		input[strcspn(input, "\n")] = 0;
 
-		char *arg_blocks[12]; //might need to be block_num + 1 because array needs to be NULL terminated	
+		char *curr_program = strtok(input, "|");
+
+		char *next_program;
+		int other_side = 0;
+		pid_t child_pid;
+		while ((next_program = strtok(NULL, "|"))) {	
+			int redir_fd;
+			bool output_re = false;
+			bool input_re = false;
+			bool append = false;
+			char *file;
+			int pipefd[2];
+			int out_fd = 1;
+
+			//always gonna pipe because we are in while loop until *programs != NULL. Will only equal null when no pipes left
+			if (pipe(pipefd) < 0) {
+				perror("pipe() error");
+			}
+			if ((child_pid = fork()) < 0) {
+				perror("fork() error");
+			}
+			else if (child_pid == 0) { //child process	
+				char *args_in_prog[10];
+				char *commargs[10];
+				int commcount = 0;
+				char *curr_arg = strtok(curr_program, " ");
+				int num_args = 0;
+				while(curr_arg != NULL) {
+					args_in_prog[num_args] = curr_arg;
+					curr_arg = strtok(NULL, " ");
+					num_args++;
+				}
+				args_in_prog[num_args] = NULL;
+
+				if (strcmp(args_in_prog[0], "exit") == 0) { 
+					return 0;
+				}
+					
+				for (int i = 0; i < num_args; i++) {
+					printf("arg %d: %s\n", i, args_in_prog[i]);
+					switch (*args_in_prog[i]) {
+						case '>':
+							//if it isn't >> output redirection
+							if (strlen(curr_arg) == 1) {
+								output_re = true;
+								file = args_in_prog[i + 1];	
+							}
+							else { //if it is >>
+								append = true;
+								file = args_in_prog[i + 1];	
+								i++; //I think this would be necessary to skip the next '>'
+							}
+							break;
+						case '<':
+							printf("input\n");
+							input_re = true;
+							file = args_in_prog[i + 1];
+							break;
+						default:
+							if (output_re || input_re || append) {
+								goto exit_loop;
+							}
+
+							commargs[commcount] = args_in_prog[i];
+							commcount += 1;
+							break;
+					}
+				}
+				exit_loop: ;
+				
+				commargs[commcount] = NULL;
+
+				out_fd = pipefd[1];
+
+				if (dup2(out_fd, 1) < 0) {
+					perror("dup2() error");
+				}
+				printf("other_side: %d\n", other_side);
+				if (dup2(other_side, 0) < 0) {
+					perror("dup2() error");
+				}
+				if (out_fd != 1) {
+					close(out_fd);
+				}
+
+				if (output_re) {
+					printf("output re\n");
+					if ((redir_fd = open(file, O_WRONLY | O_CREAT, 0777)) < 0) {
+						perror("open() error");
+						exit(42);
+					}
+					printf("redir_fd: %d\n", redir_fd);
+					if (dup2(redir_fd, 1) < 0) {
+						perror("dup2() error");
+						exit(42);
+					}
+					if (close(redir_fd) < 0) {
+						perror("close() error");
+						exit(42);
+					}
+				}
+				if (input_re) {
+					if ((redir_fd = open(file, O_RDONLY | O_CREAT, 0777)) < 0) {
+						perror("open() error");
+						exit(42);
+					}
+					if (dup2(redir_fd, 0) < 0) {
+						perror("dup2() error");
+						exit(42);
+					}
+					if (close(redir_fd) < 0) {
+						perror("close() error");
+						exit(42);
+					}
+				}
+				if (append) {
+					printf("append re\n");
+					if ((redir_fd = open(file, O_WRONLY | O_CREAT | O_APPEND, 0777)) < 0) {
+						perror("open() error");
+						exit(42);
+					}
+					if (dup2(redir_fd, 1) < 0) {
+						perror("dup2() error");
+						exit(42);
+					}
+					if (close(redir_fd) < 0) {
+						perror("close() error");
+						exit(42);
+					}
+				}
+				if ((execvp(args_in_prog[0], commargs)) < 0) {
+					perror("Not a valid command");
+					errno = ENOENT;
+					exit(42);
+				}
+			}
+			else { //parent process		
+				if (out_fd != 1) {
+					if (close(out_fd) < 0) {
+						perror("close() error");
+					}
+				}
+
+				other_side = pipefd[0];
+			}
+
+			
+			curr_program = next_program;
+			num_programs++;
+		}
+
+		printf("out of here\n");
+		//one last program
+		int redir_fd;
+		bool output_re = false;
+		bool input_re = false;
+		bool append = false;
+		char *file;
+		if ((child_pid = fork()) < 0) {
+			perror("fork() error");
+		}
+		else if (child_pid == 0) { //child process	
+			char *args_in_prog[10];
+			char *commargs[10];
+			int commcount = 0;
+			char *curr_arg = strtok(curr_program, " ");
+			int num_args = 0;
+			while(curr_arg != NULL) {
+				args_in_prog[num_args] = curr_arg;
+				curr_arg = strtok(NULL, " ");
+				num_args++;
+			}
+			args_in_prog[num_args] = NULL;
+
+			if (strcmp(args_in_prog[0], "exit") == 0) { 
+				return 0;
+			}
+				
+			for (int i = 0; i < num_args; i++) {
+				printf("arg %d: %s\n", i, args_in_prog[i]);
+				switch (*args_in_prog[i]) {
+					case '>':
+						//if it isn't >> output redirection
+						if (strlen(curr_arg) == 1) {
+							output_re = true;
+							file = args_in_prog[i + 1];	
+						}
+						else { //if it is >>
+							append = true;
+							file = args_in_prog[i + 1];	
+							i++; //I think this would be necessary to skip the next '>'
+						}
+						break;
+					case '<':
+						printf("input\n");
+						input_re = true;
+						file = args_in_prog[i + 1];
+						break;
+					default:
+						if (output_re || input_re || append) {
+							goto exit_loop2;
+						}
+
+						commargs[commcount] = args_in_prog[i];
+						commcount += 1;
+						break;
+				}
+			}
+			exit_loop2: ;
+			
+			commargs[commcount] = NULL;
+
+			printf("other_side: %d\n", other_side);
+			if (dup2(other_side, 0) < 0) {
+				perror("dup2() error");
+			}
+
+			if (output_re) {
+				printf("output re\n");
+				if ((redir_fd = open(file, O_WRONLY | O_CREAT, 0777)) < 0) {
+					perror("open() error");
+					exit(42);
+				}
+				printf("redir_fd: %d\n", redir_fd);
+				if (dup2(redir_fd, 1) < 0) {
+					perror("dup2() error");
+					exit(42);
+				}
+				if (close(redir_fd) < 0) {
+					perror("close() error");
+					exit(42);
+				}
+			}
+			if (input_re) {
+				if ((redir_fd = open(file, O_RDONLY | O_CREAT, 0777)) < 0) {
+					perror("open() error");
+					exit(42);
+				}
+				if (dup2(redir_fd, 0) < 0) {
+					perror("dup2() error");
+					exit(42);
+				}
+				if (close(redir_fd) < 0) {
+					perror("close() error");
+					exit(42);
+				}
+			}
+			if (append) {
+				printf("append re\n");
+				if ((redir_fd = open(file, O_WRONLY | O_CREAT | O_APPEND, 0777)) < 0) {
+					perror("open() error");
+					exit(42);
+				}
+				if (dup2(redir_fd, 1) < 0) {
+					perror("dup2() error");
+					exit(42);
+				}
+				if (close(redir_fd) < 0) {
+					perror("close() error");
+					exit(42);
+				}
+			}
+			if ((execvp(args_in_prog[0], commargs)) < 0) {
+				perror("Not a valid command");
+				errno = ENOENT;
+				exit(42);
+			}
+		}
+		else { //parent process		
+
+		}
+
 		
-		char *pipe_tok = strtok(input, "|");
-		int num_blocks = 0;
 
-		while (pipe_tok != NULL) {
-			arg_blocks[num_blocks] = pipe_tok;
-			pipe_tok = strtok(NULL, "|");
-			num_blocks++;
+		for (int i = 0; i < num_programs; i++) {
+			if (wait(NULL) < 0) {
+				perror("wait() error");
+			}
+			//free(final_blocks[i]);
 		}
 
-		struct block *final_blocks[12];
+		printf("out of loop\n");
+	}
+}
 
-		for (int i = 0; i < 12; i++) {
-			struct block *new_block = malloc(sizeof(struct block)); //need to allocate mem for this
-			new_block->num_args = 0;
-			new_block->num_comm = 0;
-			new_block->output_re = false;
-			new_block->append = false;
-			new_block->input_re = false;
-			final_blocks[i] = new_block;
+int execute_program(char *curr_program, int other_side) {
+	int redir_fd;
+	bool output_re = false;
+	bool input_re = false;
+	bool append = false;
+	char *file;
+	int pipefd[2];
+	int out_fd = 1;
+
+//always gonna pipe because we are in while loop until *programs != NULL. Will only equal null when no pipes left
+	if (pipe(pipefd) < 0) {
+		perror("pipe() error");
+	}
+	pid_t child_pid;
+
+	if ((child_pid = fork()) < 0) {
+		perror("fork() error");
+	}
+	else if (child_pid == 0) { //child process	
+		char *args_in_prog[10];
+		char *commargs[10];
+		int commcount = 0;
+		char *curr_arg = strtok(curr_program, " ");
+		int num_args = 0;
+		while(curr_arg != NULL) {
+			args_in_prog[num_args] = curr_arg;
+			curr_arg = strtok(NULL, " ");
+			num_args++;
+		}
+		args_in_prog[num_args] = NULL;
+
+		if (strcmp(args_in_prog[0], "exit") == 0) { 
+			return 0;
+		}
+			
+		for (int i = 0; i < num_args; i++) {
+			printf("arg %d: %s\n", i, args_in_prog[i]);
+			switch (*args_in_prog[i]) {
+				case '>':
+					//if it isn't >> output redirection
+					if (strlen(curr_arg) == 1) {
+						output_re = true;
+						file = args_in_prog[i + 1];	
+					}
+					else { //if it is >>
+						append = true;
+						file = args_in_prog[i + 1];	
+						i++; //I think this would be necessary to skip the next '>'
+					}
+					break;
+				case '<':
+					printf("input\n");
+					input_re = true;
+					file = args_in_prog[i + 1];
+					break;
+				default:
+					if (output_re || input_re || append) {
+						goto exit_loop;
+					}
+
+					commargs[commcount] = args_in_prog[i];
+					commcount += 1;
+					break;
+			}
+		}
+		exit_loop: ;
+		
+		commargs[commcount] = NULL;
+
+		out_fd = pipefd[1];
+
+		if (dup2(out_fd, 1) < 0) {
+			perror("dup2() error");
+		}
+		printf("other_side: %d\n", other_side);
+		if (dup2(other_side, 0) < 0) {
+			perror("dup2() error");
+		}
+		if (out_fd != 1) {
+			close(out_fd);
 		}
 
+		if (output_re) {
+			printf("output re\n");
+			if ((redir_fd = open(file, O_WRONLY | O_CREAT, 0777)) < 0) {
+				perror("open() error");
+				exit(42);
+			}
+			printf("redir_fd: %d\n", redir_fd);
+			if (dup2(redir_fd, 1) < 0) {
+				perror("dup2() error");
+				exit(42);
+			}
+			if (close(redir_fd) < 0) {
+				perror("close() error");
+				exit(42);
+			}
+		}
+		if (input_re) {
+			if ((redir_fd = open(file, O_RDONLY | O_CREAT, 0777)) < 0) {
+				perror("open() error");
+				exit(42);
+			}
+			if (dup2(redir_fd, 0) < 0) {
+				perror("dup2() error");
+				exit(42);
+			}
+			if (close(redir_fd) < 0) {
+				perror("close() error");
+				exit(42);
+			}
+		}
+		if (append) {
+			printf("append re\n");
+			if ((redir_fd = open(file, O_WRONLY | O_CREAT | O_APPEND, 0777)) < 0) {
+				perror("open() error");
+				exit(42);
+			}
+			if (dup2(redir_fd, 1) < 0) {
+				perror("dup2() error");
+				exit(42);
+			}
+			if (close(redir_fd) < 0) {
+				perror("close() error");
+				exit(42);
+			}
+		}
+		if ((execvp(args_in_prog[0], commargs)) < 0) {
+			perror("Not a valid command");
+			errno = ENOENT;
+			exit(42);
+		}
+	}
+	else { //parent process		
+		if (out_fd != 1) {
+			if (close(out_fd) < 0) {
+				perror("close() error");
+			}
+		}
+
+		other_side = pipefd[0];
+	}
+
+	return other_side;
+}
+
+
+		/*
 		char *space_tok;
 		
 		//char *args[12];
@@ -134,89 +535,13 @@ int main(int argc, char *argv[]) {
 			}
 		
 			//do all the forking first and then all the waiting	
-			if ((child_pid = fork()) < 0) {
-				perror("fork() error");
-			}
-			else if (child_pid == 0) { //child process
-				
-				if (dup2(out_fd, 1) < 0) {
-					perror("dup2() error");
-				}
-				if (dup2(other_side, 0) < 0) {
-					perror("dup2() error");
-				}
-				if (out_fd != 1) {
-					close(out_fd);
-				}
-
-				if (final_blocks[i]->output_re) {
-					printf("output re\n");
-					if ((redir_fd = open(final_blocks[i]->file, O_RDWR | O_CREAT, 0777)) < 0) {
-						perror("open() error");
-						exit(42);
-					}
-					printf("redir_fd: %d\n", redir_fd);
-					if (dup2(redir_fd, 1) < 0) {
-						perror("dup2() error");
-						exit(42);
-					}
-					if (close(redir_fd) < 0) {
-						perror("close() error");
-						exit(42);
-					}
-				}
-				if (final_blocks[i]->input_re) {
-					if ((redir_fd = open(final_blocks[i]->file, O_RDWR | O_CREAT, 0777)) < 0) {
-						perror("open() error");
-						exit(42);
-					}
-					if (dup2(redir_fd, 0) < 0) {
-						perror("dup2() error");
-						exit(42);
-					}
-					if (close(redir_fd) < 0) {
-						perror("close() error");
-						exit(42);
-					}
-				}
-				if (final_blocks[i]->append) {
-					printf("append re\n");
-					if ((redir_fd = open(final_blocks[i]->file, O_RDWR | O_CREAT | O_APPEND, 0777)) < 0) {
-						perror("open() error");
-						exit(42);
-					}
-					if (dup2(redir_fd, 1) < 0) {
-						perror("dup2() error");
-						exit(42);
-					}
-					if (close(redir_fd) < 0) {
-						perror("close() error");
-						exit(42);
-					}
-				}
-				if ((execvp(final_blocks[i]->args[0], final_blocks[i]->commargs)) < 0) {
-					perror("Not a valid command");
-					errno = ENOENT;
-					exit(42);
-				}
-			}
-			else { //parent process		
-				if (out_fd != 1) {
-					if (close(out_fd) < 0) {
-						perror("close() error");
-					}
-				}
-				other_side = pipefd[0];
-			}
 		}
+	*/
 
+		/*
 		for (int i = 0; i < num_blocks; i++) {
 			if (wait(NULL) < 0) {
 				perror("wait() error");
 			}
-		}
-
-	}
-}
-
-
+			//free(final_blocks[i]);
+		}*/
